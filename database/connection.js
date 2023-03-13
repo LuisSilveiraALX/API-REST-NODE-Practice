b2857e654134b10789675c6570040a2ab092f9ca
Luis <lswagames@gmail.com>
2023-03-13 15:49:56 -0300

    Create server to Nodejs

--- database/connection.js
+++ database/connection.js
@@ -5,10 +5,15 @@ const connection = async() => {
     try {
 
         mongoose.connect('mongodb+srv://lsgaming98:cocacola123@cluster0.l7ixjts.mongodb.net/my_blog')
+        console.log('Correct connection my_blog')
 
     } catch (error) {
         console.log(error);
         throw new Error('Database connection error')
     }
 
+}
+
+module.exports = {
+    connection
 }
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { connection } = require('./database/connection')
+const express = require('express');
+const cors = require('cors');
+
+// Start APP
 console.log('Hello, World')
+
+// Connect to database
+connection()
+
+// Create Server Node
+const app = express();
+const port = 3001;
+
+// Cors
+ app.use(cors()); 
+
+// Convert body to JS object
+app.use(express.json());
+
+// Create Rutes
+
+// Create server and listen requests HTTP
+app.listen(port, () => {
+    console.log(`Server run on port ${port}`)
+})
